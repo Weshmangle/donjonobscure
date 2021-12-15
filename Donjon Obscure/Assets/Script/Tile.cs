f63ee1080f1517b652d52c58380d112a6f920e0b
weshmangle <weshmangle@gmail.com>
2021-12-15 08:51:39 +0100

    no message

---
+++ Donjon Obscure/Assets/Script/Tile.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject content = null;
+     [SerializeField]
+    protected UnityEvent uEvent;
+    
+    void Start()
+    {
+        if(content)
+        {
+            Instantiate(content, new Vector3(0,.25f,0), Quaternion.Euler(0,0,0), transform);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        uEvent.Invoke();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void setEvent(UnityAction action)
+    {
+        uEvent.AddListener(action);
+    }
+}
