e0d0d5503dd4dd8c729dc0a016cc99b9562921cc
Loup-Venant <laurent.harold@gmail.com>
2021-12-15 12:45:36 +0100

    + Enum Stat + UpdateStat() + KillEntity() + TakeDamage() + Attack()

--- Donjon Obscure/Assets/Script/Entity.cs
+++ Donjon Obscure/Assets/Script/Entity.cs
@@ -1,7 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Stat
+{
+    HealthPoint,
+    HealthPointMax,
+    AttackStrength,
+    ArmorPoint,
+    ArmorPointMax,
+    MentalSanity,
+    MentalSanityMax,
+}
 public abstract class Entity : MonoBehaviour
 {
 
@@ -9,11 +20,58 @@ public abstract class Entity : MonoBehaviour
     protected int healthPointMax;
     
     protected int attackStrenght;
-    protected int attackStrenghtMax;
 
-    public virtual void Die()
+
+    protected virtual void Move(Vector2 tilePosition)
     {
-        healthPoint = 0;
+        transform.position = tilePosition;
+    }
+    protected virtual void Attack(Entity target)
+    {
+        target.TakeDamage(attackStrenght);
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        healthPoint =- damage;
+        if (healthPoint <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        Debug.Log("Entity died");
+    }
+    public virtual void KillEntity()
+    {
+        TakeDamage(healthPoint);
+    }
+
+    public virtual void UpdateStat(int addedValue, Stat stat)
+    {
+        switch (stat)
+        {
+            case Stat.HealthPoint:
+                {
+                    healthPoint += addedValue;
+                }
+                break;
+            case Stat.HealthPointMax:
+                {
+                    healthPointMax += addedValue;
+                }
+                break;
+            case Stat.AttackStrength:
+                {
+                    attackStrenght += addedValue;
+                }
+                break;
+            default:
+                Debug.Log("Provided stat does not exist.");
+                break;
+        }
     }
         
 }
