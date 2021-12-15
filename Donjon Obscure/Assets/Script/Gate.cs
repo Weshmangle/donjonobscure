1dae9593179689daa0c52b53864c1efcb586cef2
pampppp <59504030+pampppp@users.noreply.github.com>
2021-12-16 00:33:40 +0100

    enum itemtype

--- Donjon Obscure/Assets/Script/Gate.cs
+++ Donjon Obscure/Assets/Script/Gate.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Gate : MonoBehaviour
 {
-    protected string type;
-    
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -15,6 +14,5 @@ public class Item : MonoBehaviour
     void Update()
     {
         
-
     }
 }
