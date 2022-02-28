c7d53829d23eba16bc1b27c9bc85c1d5376565d8
Loup-Venant <laurent.harold@gmail.com>
2022-02-28 23:15:04 +0100

    Removed Character animation over check

--- Donjon Obscure/Assets/Script/Game.cs
+++ Donjon Obscure/Assets/Script/Game.cs
@@ -198,7 +198,7 @@ public class Game : MonoBehaviour
 //character.IsAnimationOver = true
     void CheckTileContent(Tile tile)
     {
-        if(tileIsClickable(tile) && character.IsAnimationOver())
+        if(tileIsClickable(tile) /*&& character.IsAnimationOver()*/)
         {
             tileCliked = tile;
 
